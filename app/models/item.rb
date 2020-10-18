171d046b3c0a281eaedf0b9691ca2f5ea52f963b
Daniel Lessenden <dless27@gmail.com>
2020-10-18 16:14:35 -0500

    Delete commented out notes

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,4 @@ class Item < ApplicationRecord
     end
     Item.import(items)
   end
-  #need to call this by using rails c
-  #and then run Item.import_items--in the development DB on postico
-  #Merchant.import_merchants
 end
