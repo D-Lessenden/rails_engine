6e5970ee2c189d627159f499d535791e2a90e924
Daniel Lessenden <dless27@gmail.com>
2020-10-19 23:42:49 -0500

    Edit the render json logic

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,38 +1,28 @@
 class Api::V1::ItemsController < ApplicationController
   def index
     render json: ItemSerializer.new(Item.all)
-    # Item.all
   end
 
   def show
-    # render json: Item.find(params[:id])
     render json: ItemSerializer.new(Item.find(params[:id]))
   end
 
   def create
-    # render json: ItemSerializer.new(Item.create({
-    #   params[:name]
-    #   params[:description]
-    #   params[:unit_price]
-    #   params[:merchant_id]
-    #
-    #   }))
-    a = render json: Item.create(item_params)
-
+    ActiveRecord::Base.connection.reset_pk_sequence!('items')
+    Item.create(item_params)
+    render json: ItemSerializer.new(Item.last)
   end
 
   def update
-    # render json: ItemSerializer.new(Item.update(params[:id], item_params))
-    render json: Item.update(params[:id], item_params)
+    render json: ItemSerializer.new(Item.update(params[:id], item_params))
   end
 
   def destroy
-    # render json: ItemSerializer.new(Item.delete(params[:id])
-    render json: Item.delete(params[:id])
+    Item.destroy(params[:id])
   end
 
   private
   def item_params
-    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+    params.permit(:name, :description, :unit_price, :merchant_id)
   end
 end
