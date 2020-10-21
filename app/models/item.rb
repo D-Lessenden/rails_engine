a7e1f2cde08c0d29447defced0ceae9f2292f2fc
Daniel Lessenden <dless27@gmail.com>
2020-10-21 15:44:25 -0500

    Create method for multiple finder

--- app/models/item.rb
+++ app/models/item.rb
@@ -33,4 +33,14 @@ class Item < ApplicationRecord
       Item.where("#{attribute} ILIKE ?", "%#{value}%").first
     end
   end
+
+  def self.multiple_finder(attribute, value)
+    if attribute == 'unit_price'
+      Item.where("to_char(#{attribute}, '000.00') ILIKE ?", "%#{value}%")
+    elsif attribute == "created_at" || attribute == "updated_at"
+      Item.where("to_char(#{attribute}, 'yyyy-mm-dd hh:mm:ss') ILIKE ?", "%#{value}%")
+    else
+      Item.where("#{attribute} ILIKE ?", "%#{value}%")
+    end
+  end
 end
