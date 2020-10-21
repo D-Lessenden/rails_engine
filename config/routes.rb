a781fd560f5943714992fcf570a3b071e22197b0
Daniel Lessenden <dless27@gmail.com>
2020-10-20 22:11:51 -0500

    Add route for items/:id/merchant

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,11 @@ Rails.application.routes.draw do
 
   namespace :api do
    namespace :v1 do
+     namespace :items do
+       get '/:item_id/merchant', to: 'merchants#index'
+     end
      resources :items, only: [:index, :show, :create, :update, :destroy]
     end
   end
-  
+
 end
