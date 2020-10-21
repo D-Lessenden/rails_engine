f323018f34a0cc54f843886b8c9aa5ef1e7f042d
Daniel Lessenden <dless27@gmail.com>
2020-10-21 00:35:23 -0500

    Create single and multiple finder methods

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -13,4 +13,17 @@ class Merchant < ApplicationRecord
     end
     Merchant.import(merchants)
   end
+
+
+  def self.single_finder(attribute, value)
+    if attribute == "created_at" || attribute == "updated_at"
+      Merchant.where("to_char(#{attribute}, 'yyyy-mm-dd hh:mm:ss') ILIKE ?", "%#{value}%").first
+    else
+      Merchant.where("#{attribute} ILIKE ?", "%#{value}%").first
+    end
+  end
+
+  def self.multiple_finder(attribute, value)
+    Merchant.where("#{attribute} ILIKE ?", "%#{value}%")
+  end
 end
