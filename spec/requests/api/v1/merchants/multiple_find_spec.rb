cdc1e11d97af5936357d71169cabc19f1d8cd49b
Daniel Lessenden <dless27@gmail.com>
2020-10-21 15:45:30 -0500

    Add a more robust test for the merchant multiple finder

--- spec/requests/api/v1/merchants/multiple_find_spec.rb
+++ spec/requests/api/v1/merchants/multiple_find_spec.rb
@@ -14,8 +14,28 @@ describe 'can find mulitple merchants with a set of criteria' do
 
   get "/api/v1/merchants/find_all?#{attribute}=#{value}"
   expect(response).to be_successful
-
   merchants = JSON.parse(response.body, symbolize_names: true)[:data]
   expect(merchants.count).to eq(2)
+  expect(merchants.first[:attributes][:name]).to eq(merchant4.name)
+  expect(merchants.last[:attributes][:name]).to eq(merchant5.name)
+  end
+
+  it 'can find matches with a created or updated at' do
+  merchant1 = create(:merchant, name: "walmart", updated_at: "2012-03-27 14:54:03")
+  merchant2 = create(:merchant, name: "target", updated_at: "2013-03-27 14:54:03")
+  merchant3 = create(:merchant, name: "king sooper's", updated_at: "2014-03-27 14:54:03")
+  merchant4 = create(:merchant, name: "home depot", updated_at: "2015-03-27 14:54:03")
+  merchant5 = create(:merchant, name: "flower depot", updated_at: "2015-03-27 14:54:03")
+
+
+  attribute = "updated_at"
+  value = "2015"
+
+  get "/api/v1/merchants/find_all?#{attribute}=#{value}"
+  expect(response).to be_successful
+  merchants = JSON.parse(response.body, symbolize_names: true)[:data]
+  expect(merchants.count).to eq(2)
+  expect(merchants.first[:attributes][:name]).to eq(merchant4.name)
+  expect(merchants.last[:attributes][:name]).to eq(merchant5.name)
   end
 end
