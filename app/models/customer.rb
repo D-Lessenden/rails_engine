fb2ffd8be8c509a7ede1ce5a58ce2f60c142d2f3
Daniel Lessenden <dless27@gmail.com>
2020-10-22 21:05:32 -0500

    Add spacing for readability

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,8 +3,9 @@ require 'activerecord-import'
 class Customer < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
-  
+
   has_many :invoices
+  
   def self.import_customers
     customers = []
     CSV.foreach('db/csv_data/customers.csv', headers: true) do |row|
